4dfa543693e09442fd1fde6576eee662cb02185e
samman adhikari <sammanadh@gmail.com>
2025-11-30 21:16:37 +0100

    adding banner in runtime and buildtime apps

--- apps/build-time/src/App.tsx
+++ apps/build-time/src/App.tsx
@@ -3,8 +3,19 @@ import { Navbar } from "@sammanadh/navbar"; // Using the name defined in package
 
 const App = () => {
   return (
-    <div className="content">
-      <Navbar />
+    <div>
+      <div id="sticky-banner" className="bg-green-900 sticky top-0 start-0 z-50 flex justify-between w-full p-4 border-b border-default bg-neutral-primary-soft">
+        <div className="flex items-center mx-auto">
+          <p className="flex items-center text-sm font-normal text-body">
+            <span className="inline-flex items-center justify-center w-6 h-1 shrink-0 me-2.5 bg-neutral-tertiary rounded-full">
+              <span className="text-white">BUILDTIME</span>
+            </span>
+          </p>
+        </div>
+      </div>
+      <div className="content">
+        <Navbar />
+      </div>
     </div>
   );
 };
--- apps/run-time/src/App.tsx
+++ apps/run-time/src/App.tsx
@@ -3,8 +3,19 @@ import Navbar from 'navbar';
 
 const App = () => {
   return (
-    <div className="content">
-      <Navbar />
+    <div>
+      <div id="sticky-banner" className="bg-red-900 sticky top-0 start-0 z-50 flex justify-between w-full p-4 border-b border-default bg-neutral-primary-soft">
+        <div className="flex items-center mx-auto">
+          <p className="flex items-center text-sm font-normal text-body">
+            <span className="inline-flex items-center justify-center w-6 h-1 shrink-0 me-2.5 bg-neutral-tertiary rounded-full">
+              <span className="text-white">RUNTIME</span>
+            </span>
+          </p>
+        </div>
+      </div>
+      <div className="content">
+        <Navbar />
+      </div>
     </div>
   );
 };
